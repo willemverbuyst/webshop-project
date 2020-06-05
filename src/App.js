a5262c72f5877f62256728afa23e7e5400a43000
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-05 16:13:50 +0200

    Create header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import CartPage from './pages/CartPage';
 import HomePage from './pages/HomePage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
@@ -8,7 +9,7 @@ import ProductDetailsPage from './pages/ProductDetailsPage';
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/details" component={ProductDetailsPage} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Navbar className="bg-info justify-content-between">
+      <Navbar.Brand href="/" className="text-white">
+        Awesome Webshop
+      </Navbar.Brand>
+      <div className="text-white">x products in cart</div>
+    </Navbar>
+  );
 }
