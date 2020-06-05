a29ede44fc3a9be4c23b4bd19991664c176061c0
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-05 16:01:14 +0200

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import CartPage from './pages/CartPage';
+import HomePage from './pages/HomePage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/details" component={ProductDetailsPage} />
+        <Route path="/cart" exact component={CartPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
