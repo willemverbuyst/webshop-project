9ac00913fbd29c3a86584b156587da9787d03796
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-05 16:32:14 +0200

    Add Redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import store from './store';
 import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-
   document.getElementById('root')
 );
---
+++ src/store/product/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  product: null,
+};
+
+export default function authReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
