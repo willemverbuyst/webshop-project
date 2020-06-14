706bb9cce4da09f35b9860caacefcec1a8bb7704
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-14 12:12:31 +0200

    Add dark-mode toggle button/functionality to Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Header from './components/Header';
 import CartPage from './pages/CartPage';
@@ -7,10 +8,13 @@ import HomePage from './pages/HomePage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import { selectDarkMode } from './store/darkMode/selectors';
 
 function App() {
+  const darkMode = useSelector(selectDarkMode).darkMode;
+
   return (
-    <div>
+    <div className={darkMode ? 'dark-mode' : 'light-mode'}>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
---
+++ src/store/darkMode/selectors.js
@@ -0,0 +1,3 @@
+export const selectDarkMode = (state) => {
+  return state.darkMode;
+};
